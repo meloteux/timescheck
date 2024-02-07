5f21599472c7b0e76e65114367c36d03471958cb
Mateus Melo <105939743+meloteux@users.noreply.github.com>
2024-02-07 01:16:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,13 @@
-body {
-    background-color: black;
-    color: lime;
-    font-family: "Courier New", Courier, monospace;
-    text-align: center;
-    margin: 0;
-    padding: 0;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    updateTime(); // Chama a função updateTime uma vez para exibir o horário atual imediatamente
+    setInterval(updateTime, 1000); // Atualiza as horas a cada segundo
+});
 
-.container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = currentTime;
 }
-
-h1 {
-    font-size: 10rem;
-    margin-bottom: 50px;
-    animation: glitch 1s infinite;
-}
-
-.links {
-    margin-top: 50px;
-}
-
-a {
-    margin: 0 10px;
-}
-
-@keyframes glitch {
-    2%, 64% {
-        transform: translate(2px, 0) skew(0deg);
-    }
-    4%, 60% {
-        transform: translate(-2px, 0) skew(0deg);
-    }
-    62% {
-        transform: translate(0, 0) skew(5deg);
-    }
-}
-  
